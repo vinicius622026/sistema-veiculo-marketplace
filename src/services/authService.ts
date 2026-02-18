d9f0e2a76ba76a639ab721c4bca887df0b686a21
vinicius622026 <viniciusisaias76@gmail.com>
2026-02-17 22:10:02 -0300

    Create authService.ts file in src/services

---
+++ src/services/authService.ts
@@ -0,0 +1,21 @@
+// authService.ts
+
+class AuthService {
+  login(email: string, password: string) {
+    // Logic to authenticate user
+  }
+
+  logout() {
+    // Logic to logout user
+  }
+
+  register(email: string, password: string) {
+    // Logic to register user
+  }
+
+  getCurrentUser() {
+    // Logic to get current user
+  }
+}
+
+export default new AuthService();
