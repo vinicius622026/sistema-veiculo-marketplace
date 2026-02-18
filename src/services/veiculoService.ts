477bb8e6b991bd900c1a418bec8a85a1038815e9
vinicius622026 <viniciusisaias76@gmail.com>
2026-02-17 22:18:46 -0300

    Create veiculoService.ts file in src/services directory

--- src/services/veiculoService.ts
+++ src/services/veiculoService.ts
@@ -1,31 +1,9 @@
-// veiculoService.ts
-
-// Function to get all vehicles
-export const getAllVehicles = async () => {
-    // Logic to fetch all vehicles
-};
-
-// Function to get a vehicle by ID
-export const getVehicleById = async (id: string) => {
-    // Logic to fetch a vehicle by its ID
-};
-
-// Function to create a new vehicle
-export const createVehicle = async (vehicleData: any) => {
-    // Logic to create a vehicle
-};
-
-// Function to update a vehicle
-export const updateVehicle = async (id: string, vehicleData: any) => {
-    // Logic to update a vehicle
-};
-
-// Function to delete a vehicle
-export const deleteVehicle = async (id: string) => {
-    // Logic to delete a vehicle
-};
-
-// Function to get vehicles by filter criteria
-export const getVehiclesByFilter = async (criteria: any) => {
-    // Logic to filter vehicles
-};
+class VeiculoService {
+    getAllVeiculos() { }
+    getVeiculoById(id: string) { }
+    createVeiculo(data: any) { }
+    updateVeiculo(id: string, data: any) { }
+    deleteVeiculo(id: string) { }
+    searchVeiculos(query: string) { }
+}
+export default new VeiculoService();
