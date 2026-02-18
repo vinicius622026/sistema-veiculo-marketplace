d7d2278701e1ded5e9c09fc7ce6abf1a380af366
vinicius622026 <viniciusisaias76@gmail.com>
2026-02-17 22:08:21 -0300

    Create pagamentoService.ts file

---
+++ src/services/pagamentoService.ts
@@ -0,0 +1,9 @@
+// pagamentoService.ts
+
+// This file will contain the implementation for the payment service
+
+class PagamentoService {
+    // Implementation goes here
+}
+
+export default PagamentoService;
